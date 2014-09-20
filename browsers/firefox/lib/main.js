72084235374a7ef48a146a734e431c4ebb93ba94
Achal Dave <achalddave@live.com>
2014-09-19 17:59:18 -0700

    Info panel shows on Firefox

--- browsers/firefox/lib/main.js
+++ browsers/firefox/lib/main.js
@@ -2,7 +2,7 @@ var pageMod = require("sdk/page-mod");
 
 var self = require("sdk/self");
 
-pageMod.PageMod({
+var pageModder = pageMod.PageMod({
     include: "*.facebook.com",
     contentStyleFile: self.data.url("eradicate.css"),
     contentScriptFile: [self.data.url("jquery.js"), self.data.url("eradicate.js")],
@@ -10,5 +10,13 @@ pageMod.PageMod({
         urls: {
             'info-panel.html': self.data.url('info-panel.html')
         }
-    }
+    },
+    onAttach: startListening
 });
+
+function startListening(worker) {
+  worker.port.on('requestUrl', function(url) {
+    console.log("Received request");
+    worker.port.emit(url, self.data.load(url));
+  });
+}
