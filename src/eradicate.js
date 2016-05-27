8ff292fb569421e285f8e9d8fc39c0dd5d08e708
Jordan West <jordwest@gmail.com>
2016-05-27 23:05:49 +1000

    Display errors in friendlier format in console

--- src/eradicate.js
+++ src/eradicate.js
@@ -16,42 +16,53 @@ import { createStore } from './store';
 
 var storePromise = createStore();
 
+const handleError = ( e ) => {
+	console.error( '-------------------------------------' );
+	console.error( 'Something went wrong loading News Feed Eradicator. Please take a screenshot of these details:' );
+	console.error( e.stack );
+	console.error( '-------------------------------------' );
+}
+
 // This delay ensures that the elements have been created by Facebook's
 // scripts before we attempt to replace them
 var eradicateRetry = setInterval(function(){
-	// Add News Feed Eradicator display
-	var streamContainer = document.getElementById('stream_pagelet');
-	if ( streamContainer === null || typeof streamContainer === 'undefined' ) {
-		return;
-	}
-
-	var newsFeedFound = false;
-	// Delete the stream to prevent its infinite scroll infinitely loading
-	// new stories (even though they are hidden)
-	var streamMatcher = /^topnews_main_stream/;
-	forEach(streamContainer.children, (child) => {
-		if(streamMatcher.test(child.id)) {
-			streamContainer.removeChild(child);
-
-			newsFeedFound = true;
-
-			// Exit the foreach
-			return false;
+	try {
+		// Add News Feed Eradicator display
+		var streamContainer = document.getElementById('stream_pagelet');
+		if ( streamContainer === null || typeof streamContainer === 'undefined' ) {
+			return;
 		}
-	});
 
-	if ( ! newsFeedFound ) {
-		return;
+		var newsFeedFound = false;
+		// Delete the stream to prevent its infinite scroll infinitely loading
+		// new stories (even though they are hidden)
+		var streamMatcher = /^topnews_main_stream/;
+		forEach(streamContainer.children, (child) => {
+			if(streamMatcher.test(child.id)) {
+				streamContainer.removeChild(child);
+
+				newsFeedFound = true;
+
+				// Exit the foreach
+				return false;
+			}
+		});
+
+		if ( ! newsFeedFound ) {
+			return;
+		}
+
+		var nfeContainer = document.createElement("div");
+		nfeContainer.id = "nfe-container";
+		streamContainer.appendChild(nfeContainer);
+
+		storePromise.then( ( store ) => {
+			ReactDOM.render(
+				React.createElement( Provider, { store: store, children: React.createElement( NewsFeedEradicator, null ) } ),
+				nfeContainer
+			);
+		} ).catch( handleError );
+	} catch ( e ) {
+		handleError( e );
 	}
-
-	var nfeContainer = document.createElement("div");
-	nfeContainer.id = "nfe-container";
-	streamContainer.appendChild(nfeContainer);
-
-	storePromise.then( ( store ) => {
-		ReactDOM.render(
-			React.createElement( Provider, { store: store, children: React.createElement( NewsFeedEradicator, null ) } ),
-			nfeContainer
-		);
-	} )
 }, 1000);
