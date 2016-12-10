25f95c40d65cf071d1e551c5185a008519ea3075
Jordan West <jordwest@gmail.com>
2016-12-10 14:06:59 +1000

    Fix infinite scrolling in chat popups on home page
    Closes #30

--- src/lib/disable-infinite-scroll.ts
+++ src/lib/disable-infinite-scroll.ts
@@ -5,11 +5,35 @@
 
 import isEnabled from './is-enabled';
 
-const maybeBlock = ( event ) => {
-	if ( isEnabled() ) {
-		event.stopImmediatePropagation();
+// Check if the event target is a chat conversation
+let isConversation = ( target ) => {
+	if ( ! target || ! target.matches ) {
 		return false;
 	}
+
+	if ( target.matches( '.conversation' ) || target.matches( '#ChatTabsPagelet' ) ) {
+		return true;
+	}
+
+	if ( ! target.parentNode ) {
+		return false;
+	}
+
+	return isConversation( target.parentElement );
+}
+	
+const maybeBlock = ( event: MouseWheelEvent ) => {
+	if ( ! isEnabled() ) {
+		return false;
+	}
+
+	// Allow infinite scrolling of chats on the home page
+	if ( isConversation( event.target ) ) {
+		return false;
+	}
+
+	event.stopImmediatePropagation();
+	return true;
 }
 
 export default function() {
