e4086c384140d7214f0575e406adf4fdc9db8722
Ethan Adams <aethos@users.noreply.github.com>
2022-07-24 21:29:21 -0400

    Update instagram.ts (#205)
    Fix blocking instagram after page layout changed

--- src/sites/instagram.ts
+++ src/sites/instagram.ts
@@ -14,7 +14,7 @@ export function eradicate(store: Store) {
     }
 
     // Don't do anything if the UI hasn't loaded yet
-    const feed = document.querySelector('main > section');
+    const feed = document.querySelector('main');
     if (feed == null) {
       return;
     }
