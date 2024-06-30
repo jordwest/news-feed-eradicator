8c318d6c9a9cafe3548a61f3552df34f7851f451
Sean Patrick Wallace <wallace.sean111@gmail.com>
2024-06-30 16:21:35 +0200

    Rebase

--- src/lib/is-enabled.ts
+++ src/lib/is-enabled.ts
@@ -1,46 +1,46 @@
 import { Sites, Site } from '../sites';
 import { SettingsState } from '../background/store/reducer';
 import {
-	getSiteStatus,
-	SiteStatusTag,
-	SiteStatus,
+  getSiteStatus,
+  SiteStatusTag,
+  SiteStatus,
 } from '../background/store/sites/selectors';
 
 function pathMatches(path: string, patterns: string[]): boolean {
   return patterns.some(pattern => {
-      // Convert wildcard patterns to regex to accomodate for wildcards in YouTube Shorts
-      const regexPattern = new RegExp(`^${pattern.replace(/\*/g, '.*')}$`);
-      return regexPattern.test(path);
+    // Convert wildcard patterns to regex to accommodate for wildcards in YouTube Shorts
+    const regexPattern = new RegExp(`^${pattern.replace(/\*/g, '.*')}$`);
+    return regexPattern.test(path);
   });
 }
 
 export type EnabledStatus =
-	| { type: 'enabled' }
-	| { type: 'disabled' }
-	| { type: 'disabled-temporarily'; until: number };
+  | { type: 'enabled' }
+  | { type: 'disabled' }
+  | { type: 'disabled-temporarily'; until: number };
 
 export function isEnabled(state: SettingsState): boolean {
-	return enabledStatus(state).type === 'enabled';
+  return enabledStatus(state).type === 'enabled';
 }
 
 export function enabledStatus(state: SettingsState): EnabledStatus {
-	const siteStatuses = getSiteStatus(state);
-	for (let siteId of Object.keys(Sites)) {
-		let site: Site = Sites[siteId];
-		const siteStatus: SiteStatus = siteStatuses[siteId];
-		if (site.domain.find(domain => window.location.host.includes(domain)) != null) {
-			// Always disabled if the path doesn't match
-			if (site.paths.indexOf(window.location.pathname) === -1) {
-				return { type: 'disabled' };
-			}
-
-			if (siteStatus.type === SiteStatusTag.DISABLED) {
-				return { type: 'disabled' };
-			} else if (siteStatus.type === SiteStatusTag.DISABLED_TEMPORARILY) {
-				return { type: 'disabled-temporarily', until: siteStatus.until };
-			}
-			return { type: 'enabled' };
-		}
-	}
-	return { type: 'disabled' };
+  const siteStatuses = getSiteStatus(state);
+  for (let siteId of Object.keys(Sites)) {
+    let site: Site = Sites[siteId];
+    const siteStatus: SiteStatus = siteStatuses[siteId];
+    // Check if the current host matches any domain in the site's domain array
+    if (site.domain.some(domain => window.location.host.includes(domain))) {
+      // Check if the current path is in the list of paths for this site
+      if (!pathMatches(window.location.pathname, site.paths)) {
+        return { type: 'disabled' };
+      }
+      if (siteStatus.type === SiteStatusTag.DISABLED) {
+        return { type: 'disabled' };
+      } else if (siteStatus.type === SiteStatusTag.DISABLED_TEMPORARILY) {
+        return { type: 'disabled-temporarily', until: siteStatus.until };
+      }
+      return { type: 'enabled' };
+    }
+  }
+  return { type: 'disabled' };
 }
