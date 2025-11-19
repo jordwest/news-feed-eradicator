89cad3dff6738214fdbf454b8656b02172532e6b
Jordan West <jordwest@gmail.com>
2025-11-20 08:54:36 +1000

    basic eradicating

---
+++ src/types/sitelist.ts
@@ -0,0 +1,23 @@
+export type SiteList = {
+	schemaVersion: 1,
+	sites: Site[]
+};
+
+export type Site = {
+	title: string,
+	hosts: string[],
+	feed?: Feed,
+	styles: StyleGroup[]
+};
+
+export type Feed = {
+	selectors: string[],
+	insertAt: 'firstChild' | 'lastChild' | 'before' | 'after' | 'overlay' | 'overlay-fixed'
+}
+
+export type StyleGroup = {
+	id: string,
+	title: string,
+	type: 'hide' | 'remove',
+	selectors: string[],
+}
