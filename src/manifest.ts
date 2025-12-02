d07708f9b76d940a33551a2e3d5a012984526210
Jordan West <jordwest@gmail.com>
2025-12-02 21:15:29 +1000

    add youtube live chat section

--- src/manifest.ts
+++ src/manifest.ts
@@ -39,5 +39,14 @@ export default {
 			"resources": ["sitelist.json", "intercept/intercept.js"],
 			"extension_ids": [],
 		}
-	]
+	],
+	"browser_specific_settings": {
+		"gecko": {
+			"id": "@news-feed-eradicator"
+		},
+		"gecko_android": {
+			"id": "@news-feed-eradicator",
+			"strict_min_version": "113.0"
+		}
+	}
 }
