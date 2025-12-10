79c121791809eca11af251919da62f55dc17e221
Jordan West <jordwest@gmail.com>
2025-12-10 12:32:57 +1000

    remove old src

--- src-old/config.ts
+++
@@ -1,3 +0,0 @@
-export default {
-	newFeatureIncrement: 1,
-};
--- src-old/lib/generate-id.ts
+++
@@ -1,7 +0,0 @@
-export function generateID(): string {
-	let key = '';
-	while (key.length < 16) {
-		key += Math.random().toString(16).substr(2);
-	}
-	return key.substr(0, 16);
-}
