1e345cf4080cdb1bd7ecf6afae4b2a2116e208e1
Jordan West <jordwest@gmail.com>
2026-01-09 13:17:32 +1000

    beta message in about page

--- src/entrypoints/options-page/tabs/about/index.tsx
+++ src/entrypoints/options-page/tabs/about/index.tsx
@@ -1,7 +1,9 @@
 export const AboutTabContent = () => {
 	return (
-		<div>
-			<p>Coming soon</p>
+		<div class="p-4 space-y-2">
+			<p>Thanks for trying out the beta!</p>
+			<p>This page coming soon.</p>
+			<p>Check <a href="https://github.com/jordwest/news-feed-eradicator/discussions/categories/announcements">announcements</a> for more info.</p>
 		</div>
 	);
 };
