f148699f9bb440474e120efecdeae32598fc9ee0
Jordan West <jordwest@gmail.com>
2026-01-30 12:44:40 +1100

    populate about page

--- src/entrypoints/options-page/tabs/about/index.tsx
+++ src/entrypoints/options-page/tabs/about/index.tsx
@@ -2,25 +2,39 @@ import type { ParentComponent } from "solid-js";
 
 export const AboutTabContent = () => {
 	return (
-		<div class="p-4 space-y-2">
-			<p>Thanks for trying out the beta!</p>
-			<p>This page coming soon.</p>
-			<p>Check <a href="https://github.com/jordwest/news-feed-eradicator/discussions/categories/announcements">announcements</a> for more info.</p>
-			<div class="divide-y b-1">
-				<ExpandingRegion title="Section 1">
-					<p>Hello I'm a paragraph</p>
-				</ExpandingRegion>
-				<ExpandingRegion title="Section 2">
-					<p>Hello I'm a paragraph</p>
-				</ExpandingRegion>
+		<div class="p-8 space-y-4">
+				<div class="prose">
+					<p>I built News Feed Eradicator in a weekend in 2012 just to help defend myself against the addictive Facebook news feed.</p>
+					<p>I never expected it to be useful to many others, but thought I'd throw it up online anyway. What I could never have predicted was that the assault on our attention would become as egregious as it has, nor the consequences it would have on society.</p>
+					<p>We are now living in the largest for-profit psychological experiment ever conducted on humans and we need tools to defend ourselves.</p>
+				</div>
+
+				<div class="prose">
+					<h3>How to support this project</h3>
+					<p>Thanks! You can help out in a few ways:</p>
+					<ul>
+						<li>Leave a review on the <a href="https://chrome.google.com/webstore/detail/news-feed-eradicator-for/fjcldmjmjhkklehbacihaiopjklihlgg?hl=en">Chrome web store</a> or <a href="https://addons.mozilla.org/en-US/firefox/addon/news-feed-eradicator/">Firefox store</a></li>
+						<li>Tell your friends about the extension</li>
+						<li><a href="https://gumroad.com/l/news-feed-eradicator">Buy me a coffee</a></li>
+						<li>Contribute bug fixes <a href="https://github.com/jordwest/news-feed-eradicator">on GitHub</a></li>
+					</ul>
+				</div>
+
+				<div class="prose">
+				<h3>I want to suggest a new site</h3>
+				<p>Submit or upvote suggestions for new sites on the <a href="https://github.com/jordwest/news-feed-eradicator/blob/version/2.x/src/components/info-panel.ts">GitHub discussion board</a>.</p>
+				</div>
+
+				<div class="prose">
+					<h3>I want to suggest a feature</h3>
+					<p>If you have an idea, please submit it (or upvote it if it already exists) on the <a href="https://github.com/jordwest/news-feed-eradicator/blob/version/2.x/src/components/info-panel.ts">GitHub Ideas discussion board</a>.</p>
+					<p>Generally I rarely add new features as this is a spare time project, I consider it mostly done, and I want to keep it as simple as possible, however I do read and consider all of these.</p>
+				</div>
+
+				<div class="prose">
+					<h3>A site isn't working or I found a bug</h3>
+					<p>Please report bugs <a href="https://github.com/jordwest/news-feed-eradicator/issues">as an issue on GitHub</a></p>
+				</div>
 			</div>
-		</div>
 	);
 };
-
-const ExpandingRegion: ParentComponent<{title: string}> = ({ title, children }) => {
-	return <details class="">
-		<summary class="hoverable cursor-pointer block p-4 flex gap-2"><div class="open-rotate"></div> {title}</summary>
-		<div class="p-4">{children}</div>
-	</details>
-}
